99e28d740172386528dcae054866f53f71c27f6b
apslagle <apslagle@gmail.com>
2017-02-17 14:17:12 -0600

    add controller spec template

---
+++ spec/controllerSpec.js
@@ -0,0 +1,30 @@
+/*
+Use below to template your tests
+
+
+describe('yourController', function() {
+  beforeEach(module('app'));
+
+  var $controller;
+
+  beforeEach(inject(function(_$controller_){
+    // The injector unwraps the underscores (_) from around the parameter names when matching
+    $controller = _$controller_;
+  }));
+
+  describe('your function', function() {
+    var $scope, controller;
+
+    beforeEach(function() {
+      $scope = {};
+      controller = $controller('PasswordController', { $scope: $scope });
+    });
+
+    it('does this', function() {
+
+      expect($something).toEqual('a thing');
+    });
+  });
+});
+
+*/
