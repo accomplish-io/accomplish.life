142e3f758ae0740605f1432c420f118c2b4ec11a
apslagle <apslagle@gmail.com>
2017-02-17 09:44:01 -0600

    set up basic server

---
+++ server/server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var bodyParser= require('body-parser');
+var path= require('path');
+//var db = require('');
+var app = express();
+var port = process.env.PORT || 3021;
+
+if(!process.env.PORT) {
+  var morgan = require('morgan');
+  app.use(morgan('dev'));
+}
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/../client'));
+app.use(express.static(__dirname + '/../node_modules'));
+
+//require('./config/routes.js')(app, express, db);
+
+app.listen(port, function() {
+  console.log('Listening on ' + port);
+});
+
+module.exports = {
+  app: app,
+};
