be7b0ad3b240c94cff832509820837e5d5cf964f
Elizabeth Akerman <ekakerman@gmail.com>
2017-02-17 12:33:05 -0800

    add user table fields

--- server/db/db-setup.js
+++ server/db/db-setup.js
@@ -2,6 +2,19 @@ var Sequelize = require('sequelize');
 var db = new Sequelize('users', 'goals', '');
 
 var User = db.define('User', {
+  nameFirst: Sequelize.STRING,
+  nameLast: Sequelize.STRING,
+  authId: Sequelize.STRING,
+  email: Sequelize.STRING,
+  phone: Sequelize.STRING,
+  public: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: true
+  },
+  start: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW
+  }
 });
 
 var Goal = db.define('Goal', {
