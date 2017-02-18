34e73f33960361c719496f80b04a045a9ca3e4a8
Eric Forsell <efor202@gmail.com>
2017-02-17 22:10:58 -0500

    Change require database

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser= require('body-parser');
 var path= require('path');
-var db = require('./db.js');
+var db = require('./db/db-connection.js');
 var app = express();
 var port = process.env.PORT || 3021;
 
