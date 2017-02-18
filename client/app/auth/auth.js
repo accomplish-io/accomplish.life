950cf7bfe7f4c25e26bd2a1bed95faa7962c51c0
Elizabeth Akerman <ekakerman@gmail.com>
2017-02-17 17:27:06 -0800

    inject authService dependency

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,6 +1,8 @@
 angular.module('auth', [])
-.controller('AuthCtrl', function(authService) {
-    var vm = this;
-    vm.authService = authService;
-});
+.controller('AuthCtrl', AuthCtrl);
+AuthCtrl.$inject = ['authService'];
 
+function AuthCtrl(authService) {
+  var vm = this;
+  vm.authService = authService;
+}
