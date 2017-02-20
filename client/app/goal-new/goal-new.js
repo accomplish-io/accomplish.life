47b47da1a425533206c98008402dcde75b416395
Daniel Ritchie <danjumamilhaus@gmail.com>
2017-02-20 13:11:27 -0600

    Comment out findUser, replace with findOrCreate, Comment out validation

--- client/app/goal-new/goal-new.js
+++ client/app/goal-new/goal-new.js
@@ -4,12 +4,21 @@ angular.module('new', [])
 
   vm.goal = '';
 
+  // lock.getProfile(localStorage.getItem('id_token'), function (error, profile) {
+  //   vm.payload = profile;
+  //   GoalFactory.findUser(vm.payload.email)
+  //     .then(user => {
+  //       vm.user = user.data;
+  //     })
+  // });
+
   lock.getProfile(localStorage.getItem('id_token'), function (error, profile) {
     console.log('profile ', profile);
     vm.payload = profile;
-    GoalFactory.findUser(vm.payload.email)
+    console.log(profile);
+    GoalFactory.findOrCreateUser(vm.payload.name, vm.payload.email)
       .then(user => {
-        vm.user = user.data;
+        vm.user = user.data[0]
       })
   });
 });
