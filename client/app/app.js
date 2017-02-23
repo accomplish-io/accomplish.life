d01a475eb3381797e938fb38d0ab4a30cb1a85b8
Daniel Ritchie <danjumamilhaus@gmail.com>
2017-02-22 22:57:39 -0600

    Update app.js

--- client/app/app.js
+++ client/app/app.js
@@ -13,57 +13,57 @@ angular.module('app', [
 config.$inject = ['$stateProvider', 'lockProvider', '$urlRouterProvider'];
 
 
-  function config($stateProvider, lockProvider, $urlRouterProvider) {
+function config($stateProvider, lockProvider, $urlRouterProvider) {
 
-    $stateProvider
-      .state('home', {
-        url: '/home',
-        controller: 'DashboardCtrl',
-        templateUrl: './app/dashboard/dashboard.html',
-        controllerAs: 'vm'
-      })
-      .state('auth', {
-        url: '/auth',
-        controller: 'AuthCtrl',
-        templateUrl: './app/auth/auth.html',
-        controllerAs: 'vm'
-      })
-      .state('new', {
-        url: '/new',
-        controller: 'NewCtrl',
-        templateUrl: './app/goal-new/goal-new.html',
-        controllerAs: 'vm'
-      })
-  // TODO: add goal :id to route?
-      .state('complete', {
-        url: '/auth',
-        controller: 'CompleteCtrl',
-        templateUrl: './app/goal-complete/goal-complete.html',
-        controllerAs: 'vm'
-      })
-  // TODO: add goal :id to route?
-      .state('details', {
-        url: '/auth',
-        controller: 'DetailsCtrl',
-        templateUrl: './app/goal-details/goal-details.html',
-        controllerAs: 'vm'
-      });
-
-    lockProvider.init({
-      clientID: AUTH0_CLIENT_ID,
-      domain: AUTH0_DOMAIN
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      controller: 'DashboardCtrl',
+      templateUrl: './app/dashboard/dashboard.html',
+      controllerAs: 'vm'
+    })
+    .state('auth', {
+      url: '/auth',
+      controller: 'AuthCtrl',
+      templateUrl: './app/auth/auth.html',
+      controllerAs: 'vm'
+    })
+    .state('new', {
+      url: '/new',
+      controller: 'NewCtrl',
+      templateUrl: './app/goal-new/goal-new.html',
+      controllerAs: 'vm'
+    })
+    // TODO: add goal :id to route?
+    .state('complete', {
+      url: '/auth',
+      controller: 'CompleteCtrl',
+      templateUrl: './app/goal-complete/goal-complete.html',
+      controllerAs: 'vm'
+    })
+    // TODO: add goal :id to route?
+    .state('details', {
+      url: '/auth',
+      controller: 'DetailsCtrl',
+      templateUrl: './app/goal-details/goal-details.html',
+      controllerAs: 'vm'
     });
 
-    //  jwtOptionsProvider.config({
-    //   tokenGetter: ['options', function (options) {
-    //     if (options && options.url.substr(options.url.length - 5) == '.html') {
-    //       return null;
-    //     }
-    //     return localStorage.getItem('id_token');
-    //   }],
-    //   whiteListedDomains: ['localhost'],
-    //   unauthenticatedRedirectPath: '/auth'
-    // });
+  lockProvider.init({
+    clientID: AUTH0_CLIENT_ID,
+    domain: AUTH0_DOMAIN
+  });
 
-    $urlRouterProvider.otherwise('/home');
-  }
+  //  jwtOptionsProvider.config({
+  //   tokenGetter: ['options', function (options) {
+  //     if (options && options.url.substr(options.url.length - 5) == '.html') {
+  //       return null;
+  //     }
+  //     return localStorage.getItem('id_token');
+  //   }],
+  //   whiteListedDomains: ['localhost'],
+  //   unauthenticatedRedirectPath: '/auth'
+  // });
+
+  $urlRouterProvider.otherwise('/home');
+};
