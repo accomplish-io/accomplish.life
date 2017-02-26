d44019c0808f1f063086a493eb95228a97692fa6
Daniel Ritchie <danjumamilhaus@gmail.com>
2017-02-25 19:30:32 -0600

    Create worker oice

---
+++ server/workers.js
@@ -0,0 +1,13 @@
+const autoComplete = (progresses) => {
+
+}
+
+module.exports = {
+  autoComplete: autoComplete
+}
+
+// o: if progress >= goal number, make goal complete true
+// i: array of objects with a number param
+// c: none
+// e: [2,4,3,1,4,2] && goal.number is 20 === still incomplete
+//    [2,4,3,1,4,2] && goal.number is 16 === goal is complete
