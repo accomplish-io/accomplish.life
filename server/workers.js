a8910311dbf6079ceb1612e73b82d0cf25358e33
Daniel Ritchie <danjumamilhaus@gmail.com>
2017-02-27 14:22:02 -0600

    Get rid of oice

--- server/workers.js
+++ server/workers.js
@@ -13,9 +13,3 @@ var compareToTarget = function(progresses, target) {
 module.exports = {
   compareToTarget: compareToTarget
 }
-
-// o: if progress >= goal number, make goal complete true
-// i: array of objects with a number param
-// c: none
-// e: [2,4,3,1,4,2] && goal.number is 20 === still incomplete
-//    [2,4,3,1,4,2] && goal.number is 16 === goal is complete
