02a5e79d2beac5f024075afb8ad473b16c45f334
Daniel Ritchie <danjumamilhaus@gmail.com>
2017-02-27 19:20:00 -0600

    Update redirect to auth page when logout, home after login

--- client/app/services/auth.js
+++ client/app/services/auth.js
@@ -8,7 +8,7 @@
 
   authService.$inject = ['lock', 'authManager', '$state'];
 
-  function authService(lock, authManager, $state) {
+  function authService(lock, authManager, $stateProvider) {
 
     function login() {
       lock.show();
@@ -19,7 +19,7 @@
     function logout() {
       localStorage.removeItem('id_token');
       authManager.unauthenticate();
-      $state.go('auth');
+      $stateProvider.go('auth');
     }
 
     // Set up the logic for when a user authenticates
