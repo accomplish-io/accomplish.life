ac11a95c00236c94fffaf264ef6787c22e6b4f9e
apslagle <apslagle@gmail.com>
2017-03-04 16:40:25 -0600

    new attempt

--- server/home.js
+++ server/home.js
@@ -1,9 +1,11 @@
 module.exports = function(req, res) {
-  res.send({
+  var response = {
     "speech": "You did it!",
     "displayText": "woot woot!",
-    "data": {"things": "and such"},
+    "data": {},
     "contextOut": [],
     "source": "ME!"
-  });
+  };
+  console.log(response)
+  res.send(response);
 }
