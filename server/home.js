b253e872c2cc0e53ccaba39e6e84a8065eaa8158
apslagle <apslagle@gmail.com>
2017-03-15 18:11:15 -0500

    add comments to home handler

--- server/home.js
+++ server/home.js
@@ -13,6 +13,8 @@ module.exports = function(req, res) {
   res.send(response);
 }
 
+//responses generated below
+
 function generateQuote() {
   var quotes = [
     "The longer I live, the more I realize the impact of attitude on life. Attitude, to me, is more important than facts. It is more important than the past, than education, than money, than circumstances, than failures, than successes, than what other people think or say or do. It is more important than appearance, giftedness, or skill. It will make or break a company ... a church ... a home. The remarkable thing is we have a choice every day regarding the attitude we will embrace for that day. We cannot change the inevitable. The only thing we can do is play on the one string we have, and that is our attitude ... I am convinced that life is 10% what happens to me, and 90% how I react to it. And so it is with you ... we are in charge of our Attitudes. -Charles Swindoll",
